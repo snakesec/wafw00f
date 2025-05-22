106bfca872724f53c30af29fbbf392fda25c44df
Weidsom Nascimento <source@snakesecurity.org>
2025-05-22 12:26:59 -0700

    Update wafw00f source code to HARDBASED 5.0

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,13 +1,11 @@
 rm -rf /opt/ANDRAX/wafw00f
 
-source /opt/ANDRAX/PYENV/python3/bin/activate
-
-/opt/ANDRAX/PYENV/python3/bin/pip3 install .
+PIPX_HOME=/opt/ANDRAX/pipx PIPX_BIN_DIR=/opt/ANDRAX/pipx/bin PIPX_MAN_DIR=/opt/ANDRAX/pipx/man /opt/ANDRAX/python3.13/bin/pipx install --force .
 
 if [ $? -eq 0 ]
 then
   # Result is OK! Just continue...
-  echo "Pip install local package... PASS!"
+  echo "Force local install... PASS!"
 else
   # houston we have a problem
   exit 1
@@ -15,6 +13,3 @@ fi
 
 cp -Rf andraxbin/* /opt/ANDRAX/bin
 rm -rf andraxbin
-
-chown -R andrax:andrax /opt/ANDRAX
-chmod -R 755 /opt/ANDRAX
